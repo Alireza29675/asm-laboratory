cf54cb75b80bdf0587b375a3a18ba960914e9bfa
alireza29675 <alireza.sheikholmolouki@gmail.com>
2019-01-09 18:23:47 +0330

    Better styling of showing results

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 const { exec } = require('child_process');
 const chalk = require('chalk');
 
+const MAX_ERROR_LINE_DIGITS = 3
+
 const asmFile = 'app.playground.asm'
 const labDir = './lab'
 
 exec(`nasm -f elf32 ${asmFile} -o ${labDir}/app.o && ld -m elf_i386 ${labDir}/app.o -o ${labDir}/app`, (err) => {
+    console.log('--------------------------------------------')
     if (err) {
-        const message = err.message.split('\n').filter(errmsg => errmsg.startsWith('app.asm')).join('\n');
-        console.error(chalk`\n{bgRed.white  Compiling error!  }\n\n${message}\n`);
+        const messages = err.message.split('\n')
+                                    .filter(msg => msg.startsWith(asmFile))
+                                    .map(msg => stylizeError(msg))
+        console.error(chalk`\n{bgRed.white  ✘ }{red  ${messages.length} compiler error${messages.length > 1 ? 's' : ''} occured!  }\n\n${messages.join('\n')}\n`);
         return;
     }
     exec(`${labDir}/app`, (err, stdout, stderr) => {
-        console.log(chalk`\n{bgGreen.black  App compiled  }\n\n{bgWhite.black ${stdout || ' '}}\n`);
+        console.log(chalk`\n{bgGreen.black  ✔ }{green  Code was compiled successfully  }\n\n{bgWhite.black ${stdout || ' '}}\n`);
     })
 })
+
+const stylizeError = (error) => {
+    const parts = error.split(':')
+    const file = parts.shift().trim()
+    const line = parts.shift().trim()
+    const type = parts.shift().trim()
+    const message = parts.join(':').trim()
+    return chalk`{bgRed.white  error line ${line.padEnd(MAX_ERROR_LINE_DIGITS, ' ')}} ${message}`;
+}
